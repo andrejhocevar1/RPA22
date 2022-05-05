62ee8e6543c63f380a0e927d0aa50108d42d6c1a
andrejhocevar1 <andrej.hocevar1234@gmail.com>
2022-05-05 21:22:21 +0200

    commit 1

---
+++ ProdajalnaArtiklov/ProdajalnaArtiklov/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProdajalnaArtiklov.ProdajalnaArtiklov.Models.MvcApplication" Language="C#" %>
---
+++ VajeWebAPI/VajeWebAPI/Mentor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VajeWebAPI
+{
+    class Mentor
+    {
+        public int MenId { get; set; }
+        public string MenIme { get; set; }
+        public string MenPriimek { get; set; }
+        public string MenVloga { get; set; }
+        public string MenNaziv { get; set; }
+        public string MenUstanova { get; set; }
+        public Nullable<System.Guid> UserID { get; set; }
+    }
+}
